d39794094b4d10e121d18701c6ba69255a516417
hasnatuz-zahara <mhasnatuz@gmail.com>
2023-12-15 20:42:15 +0600

    create cart items

--- script.js
+++ script.js
@@ -3,4 +3,14 @@ const searchForm = document.querySelector('.search-form')
 
 searchIcon.addEventListener("click", () => {
 searchForm.classList.add("active");
+cartItemsContainer.classList.remove("active");
+});
+
+
+const cartIcon = document.querySelector('.cart-icon')
+const cartItemsContainer = document.querySelector('.cart-items-container')
+
+cartIcon.addEventListener("click", () => {
+cartItemsContainer.classList.add("active");
+searchForm.classList.remove("active");
 });
