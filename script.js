14daf08587e9ce78bfd0e13b87799e8b16f65608
hasnatuz-zahara <mhasnatuz@gmail.com>
2023-12-31 20:53:01 +0600

    make navigation menu adapt to different screen sizes

--- script.js
+++ script.js
@@ -1,16 +1,30 @@
-const searchIcon = document.querySelector('.search-icon')
-const searchForm = document.querySelector('.search-form')
+const searchIcon = document.querySelector('.search-icon');
+const searchForm = document.querySelector('.search-form');
+const menuIcon = document.querySelector('.menu-icon');
+const navbar = document.querySelector('.navbar');
+const cartIcon = document.querySelector('.cart-icon');
+const cartItemsContainer = document.querySelector('.cart-items-container');
 
 searchIcon.addEventListener("click", () => {
-searchForm.classList.add("active");
-cartItemsContainer.classList.remove("active");
+    searchForm.classList.add("active");
+    cartItemsContainer.classList.remove("active");
+    navbar.classList.remove("active");
 });
 
-
-const cartIcon = document.querySelector('.cart-icon')
-const cartItemsContainer = document.querySelector('.cart-items-container')
+menuIcon.addEventListener("click", () => {
+    navbar.classList.add("active");
+    searchForm.classList.remove("active");
+    cartItemsContainer.classList.remove("active");
+});
 
 cartIcon.addEventListener("click", () => {
-cartItemsContainer.classList.add("active");
-searchForm.classList.remove("active");
+    cartItemsContainer.classList.add("active");
+    searchForm.classList.remove("active");
+    navbar.classList.remove("active");
 });
+
+window.onscroll = () => {
+    cartItemsContainer.classList.remove("active");
+    searchForm.classList.remove("active");
+    navbar.classList.remove("active");
+}
